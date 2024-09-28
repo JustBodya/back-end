c13a7a08d86bb257741b6ddc51ad789a3b82ae41
JustBodya <bagdancanturk@gmail.com>
2024-09-28 18:58:13 +0300

    upgrade

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ db.init_app(app)
 CORS(app)
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-    
 
 subscribed_users = set()
 
@@ -36,9 +34,11 @@ def sitemap():
 @app.route('/webhook', methods=['POST'])
 def handle_telegram_webhook():
     data = request.get_json()
+    print("Received webhook data:", data)  # Проверка данных, получаемых от Telegram
 
     if 'message' in data and 'chat' in data['message']:
         chat_id = data['message']['chat']['id']
+        print(f"Chat ID added: {chat_id}")  # Проверка, добавляется ли chat_id
         subscribed_users.add(chat_id)
         return jsonify({"status": "ok"}), 200
     
@@ -47,6 +47,7 @@ def handle_telegram_webhook():
 
 # Функция отправки заявки в тг канал
 def send_to_telegram(message):
+    print("Sending message:", message)  # Проверка отправляемого сообщения
     success = True
     for chat_id in subscribed_users:
         url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
@@ -55,6 +56,7 @@ def send_to_telegram(message):
             'text': message
         }
         response = requests.post(url, data=data)
+        print(f"Response from Telegram for chat_id {chat_id}: {response.status_code} - {response.text}")
         if response.status_code != 200:
             success = False
     
@@ -71,8 +73,8 @@ def get_people():
 # Добавление человека и его данных в БД
 @app.route('/people', methods=['POST'])
 def post_people():
-
     data = request.get_json()
+    print("Received data for new person:", data)  # Проверка входящих данных
 
     if 'phone' not in data or 'name' not in data or 'answer' not in data:
         abort(400, description="Необходимо указать все поля!")
@@ -83,8 +85,10 @@ def post_people():
 
     message = f"Добавлен новый человек:\nИмя: {person.name}\nТелефон: {person.phone}\nВопрос: {person.answer}"
     if send_to_telegram(message):
+        print("Message sent successfully.")  # Проверка успешной отправки
         return jsonify(person.to_dict()), 201
     else:
+        print("Failed to send message.")  # Проверка неудачной отправки
         return jsonify({"error": "Не удалось отправить сообщение в Telegram"}), 500
 
 
